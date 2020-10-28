a2839da8cd34cd2038eff04f52f4f88c6da46dc4
Jenni <justlittleoldmejenn@gmail.com>
2020-10-27 17:55:34 -0700

    Login page complete, index almost done

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#myCarousel").carousel("prev");
+});
+})
